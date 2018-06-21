f0c0fbd4dbbea54dd49c03818aa3c30dd8ce5f0d
Luke Ramsay <lukram98@gmail.com>
2018-06-21 15:25:55 -0400

    Final Commit

--- javascript/game.js
+++ javascript/game.js
@@ -1,46 +1,47 @@
-var answers = ["A","C","B"],
-    tot = answers.length;
+  var answers = ["C","C","B","A","D"],
+      tot = answers.length;
 
-var totalSeconds = 10;
-var seconds = parseInt(totalSeconds%60);
+  var totalSeconds = 30;
+  var seconds = parseInt(totalSeconds%60);
 
-function getCheckedValue( radioName ){
-    var radios = document.getElementsByName( radioName );
-    console.log(radios); // Get radio group by-name
-    for(var y = 0; y < radios.length; y++)
-      if(radios[y].checked) {
-        return radios[y].value; // return the checked value
+  function getCheckedValue(radioName){
+      var radios = document.getElementsByName(radioName);
+      for(var y = 0; y < radios.length; y++){
+        if(radios[y].checked) {
+          return radios[y].value;
+        }
       }
-}
+  }
 
-function getScore(){
-  var score = 0;
-  for (var i = 0; i < tot; i++) {
-    if(getCheckedValue("question" + i) === answers[i]) {
-    score += 1; // increment only
+  function getScore(){
+    var score = 0;
+    for (var i = 0; i < tot; i++) {
+      if(getCheckedValue("question" + i) === answers[i]) {
+      score += 1;
+      }
+    }
+    return "You answered " + score + "/5 questions correct";
+  }
+
+  function returnScore(){
+    document.getElementById("results").innerHTML = getScore();
+  }
+
+  function checkTime() {
+    document.getElementById("time-left").innerHTML = "Time Left: " + seconds;
+    if(totalSeconds == 0) {
+      returnScore();
+    } else {
+      totalSeconds -= 1;
+      seconds = parseInt(totalSeconds%60);
+      setTimeout("checkTime()", 1000);
     }
   }
-  return "You answered " + score + "/10 questions correct";
-}
+  setTimeout("checkTime()", 1000);
 
-function returnScore(){
-  document.getElementById("results").innerHTML = getScore();
-}
 
-function checkTime() {
-  document.getElementById("time-left").innerHTML = "Time Left: " + seconds;
-  if(totalSeconds == 0) {
+
+  $("#submit").on("click", function () {
     returnScore();
-  } else {
-    totalSeconds -= 1;
-    seconds = parseInt(totalSeconds%60);
-    setTimeout("checkTime()", 1000);
-  }
-}
-setTimeout("checkTime()", 1000);
-
-
-$("#submit").on("click", function () {
-  returnScore();
-  window.clearTimeout(seconds);
-})
+    clearInterval(seconds);
+  });
