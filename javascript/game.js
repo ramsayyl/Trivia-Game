50f24fd98c6a5cfdf56b4833bbee6e79545cc7e1
Luke Ramsay <lukram98@gmail.com>
2018-06-21 13:32:44 -0400

    slight css updates

--- javascript/game.js
+++ javascript/game.js
@@ -7,7 +7,7 @@ var seconds = parseInt(totalSeconds%60);
 function getCheckedValue( radioName ){
     var radios = document.getElementsByName( radioName );
     console.log(radios); // Get radio group by-name
-    for(var y=0; y<radios.length; y++)
+    for(var y = 0; y < radios.length; y++)
       if(radios[y].checked) {
         return radios[y].value; // return the checked value
       }
@@ -15,18 +15,20 @@ function getCheckedValue( radioName ){
 
 function getScore(){
   var score = 0;
-  for (var i=0; i<tot; i++)
-    if(getCheckedValue("question"+i)===answers[i]) score += 1; // increment only
-  return score;
+  for (var i = 0; i < tot; i++) {
+    if(getCheckedValue("question" + i) === answers[i]) {
+    score += 1; // increment only
+    }
+  }
+  return "You answered " + score + "/10 questions correct";
 }
 
 function returnScore(){
-  alert("Your score is "+ getScore() +"/"+ tot);
+  document.getElementById("results").innerHTML = getScore();
 }
 
 function checkTime() {
   document.getElementById("time-left").innerHTML = "Time Left: " + seconds;
-
   if(totalSeconds == 0) {
     returnScore();
   } else {
@@ -40,4 +42,5 @@ setTimeout("checkTime()", 1000);
 
 $("#submit").on("click", function () {
   returnScore();
+  window.clearTimeout(seconds);
 })
